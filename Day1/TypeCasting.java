aac405fa099941b6e9c5f285ea91bc48fb345edd
Rushikesh Mangulkar <80615929+Rushi9881@users.noreply.github.com>
2023-12-23 11:14:57 +0530

    Add files via upload

---
+++ Day1/TypeCasting.java
@@ -0,0 +1,20 @@
+public class TypeCasting {
+
+    public static void main(String[] args) {
+        // widening casting
+        int myInt = 9;
+        System.out.println(myInt); 
+
+        // narrowing casting
+        double myDouble1 = 9.78;
+        System.out.println(myDouble1);
+
+        // automatic casting
+        char a = 'A';
+        int b = a;
+        System.out.println(b);
+
+     
+    }
+    
+}
---
+++ Day1/variablesDemo.java
@@ -0,0 +1,29 @@
+
+    public class variablesDemo {
+            
+        public static void main(String[] args) {
+            //String
+            String name = "Rushikesh Mangulkar";
+            
+            //int
+            int age = 23;
+
+            //float
+            float cgpa= 8.97f;
+
+            //char
+            char grade = 'A';
+
+            //boolean
+            boolean isEligible = true;
+
+            System.out.println("Name: " + name);
+            System.out.println("Age: " + age);
+            System.out.println("CGPA: " + cgpa);
+            System.out.println("Grade: " + grade);
+            System.out.println("Is Eligible: " + isEligible);
+           
+
+        }
+    }
+    
